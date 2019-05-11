a26135143eb14dd74a47fa7c91ded0342fb6836d
Prakash Pandey <prakashpandeyy@yahoo.com>
2019-05-12 00:55:43 +0530

    adding asset file

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/getlantern/systray"
+)
+
 func main() {
+	systray.Run(run, exit)
+}
+
+func run() {
+	setIcon()
+	systray.SetTitle("x-clock")
+	systray.SetTooltip("Clock application")
+}
+
+func setIcon() {
+	file := "assets/clock.ico"
+	icon, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Printf("error reading app icon file: %s : %s", file, err)
+	}
+	systray.SetIcon(icon)
+}
+
+func exit() {
 
 }
